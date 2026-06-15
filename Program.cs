7089973005dd2f329bfe4a46fb4af0bb5c15facd
EzMigzz <judielmeguiel@gmail.com>
2026-06-16 05:50:01 +0800

    Added loop structure from 1 to 100

--- Program.cs
+++ Program.cs
@@ -9,22 +9,7 @@ namespace BSIT31E1_PRELIM_A1_Mescallado_Judiel_Meguiel
             // FizzBuzz Implementation
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                // WALA LAMAN DITO MUNA
             }
         }
     }
