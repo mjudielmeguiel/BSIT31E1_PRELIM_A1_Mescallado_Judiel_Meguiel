6d72e0ce560533cab45da020ce1edecfc1ec8836
EzMigzz <judielmeguiel@gmail.com>
2026-06-16 05:43:31 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BSIT31E1_PRELIM_A1_Mescallado_Judiel_Meguiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FizzBuzz Implementation
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
