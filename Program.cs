bdc5719f4b593852b058206daa4027006cc3fb43
EzMigzz <judielmeguiel@gmail.com>
2026-06-16 05:50:59 +0800

    Implemented full FizzBuzz logic

--- Program.cs
+++ Program.cs
@@ -9,7 +9,22 @@ namespace BSIT31E1_PRELIM_A1_Mescallado_Judiel_Meguiel
             // FizzBuzz Implementation
             for (int i = 1; i <= 100; i++)
             {
-                // WALA LAMAN DITO MUNA
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
